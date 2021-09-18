4ddf5b133be5881027889241792e3c84ed33d4db
plizong <me@plizong.com>
2021-09-18 23:38:19 +0800

    Add config file - manage parameters together

---
+++ lab-1/config/config.rb
@@ -0,0 +1,14 @@
+require './utilities/cards.rb'
+
+module Config
+  # parameter of card size
+  CARD_HEIGHT = 160
+  CARD_WIDTH = 70
+
+  # parameter of Windows size
+  WINDOWS_HEIGHT = 1080
+  WINDOWS_WIDTH = 1920
+
+
+
+end
--- lab-1/startGame.rb
+++ lab-1/startGame.rb
@@ -1,4 +1,5 @@
 require 'gosu'
+require './config/config.rb'
 
 class StartPage < Gosu::Window
   def initialize
