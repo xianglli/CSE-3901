141abaae749ef5b0961f1998d7dbf11c8210938f
plizong <me@plizong.com>
2021-09-19 14:22:27 +0800

    Update the welcome page

--- lab-1/startGame.rb
+++ lab-1/startGame.rb
@@ -2,10 +2,18 @@ require 'gosu'
 require './config/config.rb'
 include Config
 
+# determines whether a graphical widget is placed over others or not
+module ZOrder
+  BACKGROUND, MIDDLE, TOP = *0..2
+end
+
 class StartPage < Gosu::Window
   def initialize
     super WINDOWS_WIDTH, WINDOWS_HEIGHT
-    self.caption = "Set Game"
+    self.caption = "Set Game by Future-Tech Corp"
+    @title = Gosu::Font.new(40)
+    @manufactor = Gosu::Font.new(20)
+    #@single= Gosu::Image.new("media/button/single_player.png")
   end
 
   def update
@@ -13,7 +21,9 @@ class StartPage < Gosu::Window
   end
 
   def draw
-    # ...
+    Gosu.draw_rect(0, 0, WINDOWS_WIDTH, WINDOWS_HEIGHT, BGCOLOR, ZOrder::BACKGROUND, mode = :default)
+    @title.draw_text("Set Card Game", WINDOWS_WIDTH / 2 - 130, LINESPACE, 0, scale_x = 1, scale_y = 1, color = 0xffffff00, mode = :default)
+    @manufactor.draw_text("by Future-Tech Corp, CSE 3901", WINDOWS_WIDTH / 2 - 130, LINESPACE * 2, 0, scale_x = 1, scale_y = 1, color = 0xff0000ff, mode = :default)
   end
 end
 
