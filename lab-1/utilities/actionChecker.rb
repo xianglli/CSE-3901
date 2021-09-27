10b57c54f2c800f93ee193ddccfba89c465a7584
ZhengmingLi123 <li.11899@osu.edu>
2021-09-26 22:54:37 -0400

    create two different level of difficulty

--- lab-1/utilities/actionChecker.rb
+++ lab-1/utilities/actionChecker.rb
@@ -27,6 +27,23 @@ module ActionChecker
     false
   end
 
+  def checker1(card1, card2, card3)
+    shape_equal = (card1.shape == card2.shape && card2.shape == card3.shape)
+    shape_diff = (card1.shape != card2.shape and card1.shape != card3.shape and card2.shape != card3.shape)
+    color_equal = (card1.color == card2.color && card2.color == card3.color)
+    color_diff = (card1.color != card2.color and card1.color != card3.color and card2.color != card3.color)
+    number_equal = (card1.number == card2.number && card2.number == card3.number)
+    number_diff = (card1.number != card2.number and card1.number != card3.number and card2.number != card3.number)
+    symbol_equal = (card1.symbol == card2.symbol && card2.symbol == card3.symbol)
+    symbol_diff = (card1.symbol != card2.symbol and card1.symbol != card3.symbol and card2.symbol != card3.symbol)
+
+    if shape_equal or color_equal or number_equal or symbol_equal
+      # if  shape_equal or color_equal or number_equal or symbol_equal
+      return true
+    end
+    false
+  end
+
   # this function is for formatting the clock
   def time_format(time1, time2)
     seconds = time2 - time1
@@ -65,8 +82,10 @@ module ActionChecker
 
     # case buttons
     if mouse_x > 100 and mouse_x < 100 + G_BUTTON_WIDTH and mouse_y > 635 and mouse_y < 635 + G_BUTTON_HEIGHT
-      "begin"
-    end
+      "beginDifficult"
+    elsif (mouse_x > 400 and mouse_x < 400 + G_BUTTON_WIDTH) and mouse_y > 635 and mouse_y < 635 + G_BUTTON_HEIGHT
+      "beginEasy"
   end
-end
+  end
+  end
 
