c197a83f4cf23e764f8d58dd3fc8d40c17f72e19
williamlovecs <67803396+williamlovecs@users.noreply.github.com>
2021-09-26 23:06:36 -0400

    Update timer.rb

--- lab-1/utilities/timer.rb
+++ lab-1/utilities/timer.rb
@@ -2,10 +2,19 @@
 # This class is powered by Shiying Chen
 
 class Timer
-  attr_accessor :mode, :time
+t = Time.new(0)
+time = 120 # the timer is set to 120 seconds
 
-  def initialize
-    @mode = 0 # 0 is counter up, 1 is counting down
-    @time = 0 # 0 stands for time out in counting down, and the initial value of counting up
-  end
+time.downto(0) do |seconds|
+  puts (t + seconds).strftime('%S')
+  sleep 1
 end
+
+end
+
+#attr_accessor :mode, :time
+
+  #def initialize
+   # @mode = 0 # 0 is counter up, 1 is counting down
+    #@time = 0 # 0 stands for time out in counting down, and the initial value of counting up
+  #end
