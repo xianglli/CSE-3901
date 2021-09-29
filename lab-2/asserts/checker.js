0293b270e9d315d0d7e361bcd326629e4fac21ab
plizong <me@plizong.com>
2021-09-29 23:36:19 +0800

    introduce jQuery, modify utilities
    Co-authored-by: ZhengmingLi123 <li.11899@osu.edu>

--- lab-2/asserts/checker.js
+++ lab-2/asserts/checker.js
@@ -1,11 +1,48 @@
-var limit = 3
-function checker(form){
-    var checkedCount=0; 
-    var checkcards = document.getElementsByName("cards");
-    for(var i=0; i<checkcards.length; i++){ 
-        if(checkbox[i].checked){ 
-           checkedCount++;  
-        } 
-    } 
-  
+function getCardId(card) {
+    let tmp = card;
+    let remainder = 0;
+    let i = 0;
+    let result = 0;
+    while (Math.floor(tmp / 3) != 0) {
+        remainder = tmp % 3;
+        result += remainder * (10 ** i);
+        i = i + 1;
+        tmp = Math.floor(tmp / 3);
+    }
+    result += tmp * (10 ** i);
+    return result;
+}
+
+getCardId(34)
+
+function getSymbol(card_id) {
+    return Math.floor(card_id / 1000);
+}
+
+function getShape(card_id) {
+    return Math.floor(card_id / 100) % 10;
+}
+
+function getColor(card_id) {
+    return Math.floor(card_id / 10) % 10;
+}
+
+function getNumber(card_id) {
+    return card_id % 10;
+}
+
+export function cardChecker(card1, card2, card3) {
+    let shape_equal = (getShape(getCardId(card1)) === getShape(getCardId(card2)) && getShape(getCardId(card2)) === getShape(getCardId(card3)));
+    let shape_diff = (getShape(getCardId(card1)) !== getShape(getCardId(card2)) && getShape(getCardId(card2)) !== getShape(getCardId(card3))
+        && getShape(getCardId(card3)) !== getShape(getCardId(card1)));
+    let color_equal = (getColor(getCardId(card1)) === getColor(getCardId(card2)) && getColor(getCardId(card2)) === getColor(getCardId(card3)));
+    let color_diff = (getColor(getCardId(card1)) !== getColor(getCardId(card2)) && getColor(getCardId(card2)) !== getColor(getCardId(card3))
+        && getColor(getCardId(card3)) !== getColor(getCardId(card1)));
+    let symbol_equal = (getSymbol(getCardId(card1)) === getSymbol(getCardId(card2)) && getSymbol(getCardId(card2)) === getSymbol(getCardId(card3)));
+    let symbol_diff = (getSymbol(getCardId(card1)) !== getSymbol(getCardId(card2)) && getSymbol(getCardId(card2)) !== getSymbol(getCardId(card3))
+        && getSymbol(getCardId(card3)) !== getSymbol(getCardId(card1)));
+    let number_equal = (getNumber(getCardId(card1)) === getNumber(getCardId(card2)) && getNumber(getCardId(card2)) === getNumber(getCardId(card3)));
+    let number_diff = (getNumber(getCardId(card1)) !== getNumber(getCardId(card2)) && getNumber(getCardId(card2)) !== getNumber(getCardId(card3))
+        && getNumber(getCardId(card3)) !== getNumber(getCardId(card1)));
+    return (shape_equal || shape_diff) && (color_equal || color_diff) && (symbol_equal || symbol_diff) && (number_equal || number_diff);
 }
--- lab-2/asserts/shuffler.js
+++ lab-2/asserts/shuffler.js
@@ -3,13 +3,13 @@ export let cardDeck;
 cardDeck = [...new Array(81).keys()];
 
 function suffle(arr) {
-    var n = arr.length;
-    var newArr = [];
-    while(n) {
-        var i = Math.floor(Math.random()*n--);
+    let n = arr.length;
+    const newArr = [];
+    while (n) {
+        const i = Math.floor(Math.random() * n--);
         newArr.push(arr.splice(i, 1)[0]);
     }
     return newArr;
- }
+}
 
 cardDeck = suffle(cardDeck);
