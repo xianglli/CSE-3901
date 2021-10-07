ae13861f65aec202cd8e49ed93cdccd442de546a
Yunqing Ma <victormyq123@gmail.com>
2021-10-07 12:13:06 -0400

    add hint botton

--- lab-2/asserts/button_listener.js
+++ lab-2/asserts/button_listener.js
@@ -1,6 +1,8 @@
 const startGameButton = document.getElementsByClassName('StartGameButton')[0];
 const stopGameButton = document.getElementsByClassName('StopGameButton')[0];
 const shufflerButton = document.getElementsByClassName('shufflerButton')[0];
+const hintButton = document.getElementsByClassName('hintButton')[0];
+
 let gameState = -1;
 let score = 0;
 
@@ -27,4 +29,8 @@ stopGameButton.addEventListener("click", function () {
 
 shufflerButton.addEventListener("click", function () {
     shuffle();
+});
+
+hintButton.addEventListener("click", function () {
+    gameAlert.innerHTML = "<div style='background-color: rgba(218,112,214,0.35); border: 1px solid crimson; color: crimson'>There are " + hint() + " sets existed</div>.";
 });
