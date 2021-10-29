f4437457e4d461de0bc9a6d1c540df41950e5660
xianglli <xianglouisli@gmail.com>
2021-10-29 19:25:49 +0800

    add user control function

--- lab-3/app/models/user.rb
+++ lab-3/app/models/user.rb
@@ -1,6 +1,19 @@
+require 'json'
+require 'net/http'
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :osuid, presence: true
+
+  def self.get_user_identity (osu_id)
+    source = 'https://directory.osu.edu/fpjson.php?name_n='+osu_id
+    resp = Net::HTTP.get_response(URI.parse(source))
+    result = resp.body
+    result = JSON.parse(result)
+    logger.debug (result)
+  end
 end
