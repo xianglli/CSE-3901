8ddfab9f7621eaf5d87be329d3b74963a83c5ca5
shiying <chen.9463@buckeyemail.osu.edu>
2021-11-18 09:44:44 -0500

    initial presentation slides and syllabus

---
+++ lab-2/asserts/torbutton.js
@@ -0,0 +1,13 @@
+function myFunction(){
+	document.getElementById("demo").innerHTML="Hello World";
+}
+    step1.onclick = myFunction () {  
+            step1.style.backgroundColor = "white";
+            step1.innerHTML = "<p style='color: white'>Step 1</p>"  
+                
+
+    }
+
+
+
+
