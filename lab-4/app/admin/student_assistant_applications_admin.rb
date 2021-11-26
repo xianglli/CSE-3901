626fdfd1c7221906c7d895f0e145416f3f8c92f8
xianglli <xianglouisli@gmail.com>
2021-11-26 22:13:27 +0800

    add model TA application

---
+++ lab-4/app/admin/student_assistant_applications_admin.rb
@@ -0,0 +1,35 @@
+Trestle.resource(:student_assistant_applications) do
+  menu do
+    item :student_assistant_applications, icon: "fa fa-star"
+  end
+
+  # Customize the table columns shown on the index view.
+  #
+  # table do
+  #   column :name
+  #   column :created_at, align: :center
+  #   actions
+  # end
+
+  # Customize the form fields shown on the new/edit views.
+  #
+  # form do |student_assistant_application|
+  #   text_field :name
+  #
+  #   row do
+  #     col { datetime_field :updated_at }
+  #     col { datetime_field :created_at }
+  #   end
+  # end
+
+  # By default, all parameters passed to the update and create actions will be
+  # permitted. If you do not have full trust in your users, you should explicitly
+  # define the list of permitted parameters.
+  #
+  # For further information, see the Rails documentation on Strong Parameters:
+  #   http://guides.rubyonrails.org/action_controller_overview.html#strong-parameters
+  #
+  # params do |params|
+  #   params.require(:student_assistant_application).permit(:name, ...)
+  # end
+end
---
+++ lab-4/db/migrate/20211126133044_create_student_assistant_applications.rb
@@ -0,0 +1,12 @@
+class CreateStudentAssistantApplications < ActiveRecord::Migration[6.1]
+  def change
+    create_table :student_assistant_applications do |t|
+      t.string :osu_id
+      t.string :courseId
+      t.text :content
+      #status including pending, approved, denied.
+      t.string :status
+      t.timestamps
+    end
+  end
+end
