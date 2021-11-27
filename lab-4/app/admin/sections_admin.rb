cff83cdbd03a75d8f54fc90d895f9c99026093a3
xianglli <xianglouisli@gmail.com>
2021-11-27 11:19:25 +0800

    fix scrap issue and add section managment part

---
+++ lab-4/app/admin/sections_admin.rb
@@ -0,0 +1,35 @@
+Trestle.resource(:sections) do
+  menu do
+    item :sections, icon: "fa fa-book"
+  end
+
+  # Customize the table columns shown on the index view.
+  #
+  # table do
+  #   column :name
+  #   column :created_at, align: :center
+  #   actions
+  # end
+
+  # Customize the form fields shown on the new/edit views.
+  #
+  # form do |section|
+  #   text_field :name
+  #
+  #   row do
+  #     col { datetime_field :updated_at }
+  #     col { datetime_field :created_at }
+  #   end
+  # end
+
+  # By default, all parameters passed to the update and create actions will be
+  # permitted. If you do not have full trust in your users, you should explicitly
+  # define the list of permitted parameters.
+  #
+  # For further information, see the Rails documentation on Strong Parameters:
+  #   http://guides.rubyonrails.org/action_controller_overview.html#strong-parameters
+  #
+  # params do |params|
+  #   params.require(:section).permit(:name, ...)
+  # end
+end
