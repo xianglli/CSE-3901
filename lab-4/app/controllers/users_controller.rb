8c586bb626de22e0ac87411a83aa142211e0b7ec
Yunqing Ma <victormyq123@gmail.com>
2021-11-28 01:40:16 -0500

    complete signup interface

--- lab-4/app/controllers/users_controller.rb
+++ lab-4/app/controllers/users_controller.rb
@@ -1,15 +1,20 @@
 class UsersController < ApplicationController
     def new
+        @user = User.new
     end
 
     def create
         @user = User.new(user_params)
-       
-        @user.save
+
+        if @user.save
+            redirect_to root_path
+          else
+            render 'new'
+        end
     end
 
     private
     def user_params
-        params.require(:user).permit(:email, :osu_id, :password)
+        params.require(:user).permit(:email, :osu_id, :password, :password_confirmation)
     end
 end
